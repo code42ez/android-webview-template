8098a1e31aad27d502c6494cb7ed90c092622e41
code42ez <snek87150@gmail.com>
2025-05-31 21:26:53 +0200

    Create MainActivity.java

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://example.com");  // URL à remplacer
+    }
+}
